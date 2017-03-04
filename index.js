6790dc64c97619bfe3403d70d6210c5fe80027ee
simpart <taria.ki4m@gmail.com>
2017-03-04 11:15:04 +0900

    support module.exports

---
+++ index.js
@@ -0,0 +1,9 @@
+require('expose-loader?ttrg!./src/namesp.js');
+require('./src/loader.js');
+require('./src/rest.js');
+require('./src/debug.js');
+require('./src/time.js');
+require('./src/url.js');
+require('./src/array.js');
+require('./src/init.js');
+module.exports = ttrg;
--- src/entry.js
+++
@@ -1,8 +0,0 @@
-require('expose-loader?ttrg!./namesp.js');
-require('./loader.js');
-require('./rest.js');
-require('./debug.js');
-require('./time.js');
-require('./url.js');
-require('./array.js');
-require('./init.js');
